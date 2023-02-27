db67c7f694313a1631ae19054de15e6376a3abf7
Dwayne McDaniel <1dwayne.mcdaniel@gmail.com>
2023-02-27 15:53:00 -0600

    slack token added

--- python_server.py
+++ python_server.py
@@ -7,6 +7,7 @@ Usage::
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import logging
 
+test_slack_key = "xoxb-563213206324-FGqsdMF8t18v6N7Oq4i90vs5"
 
 class S(BaseHTTPRequestHandler):
     def _set_response(self):
